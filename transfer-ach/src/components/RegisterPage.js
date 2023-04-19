32e5d1b2836e02b79b1d767bebe5ef77be71e883
Jean Dumas <37590971+jeand94@users.noreply.github.com>
2023-04-19 18:34:39 -0500

    Cleaned up Registration page

--- transfer-ach/src/components/RegisterPage.js
+++ transfer-ach/src/components/RegisterPage.js
@@ -15,127 +15,127 @@ address: '',
 });
 
 const handleChange = (e) => {
-setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
 };
 
 const handleRegister = async (e) => {
-e.preventDefault();
-try {
-const response = await axios.post('http://localhost:3001/api/register', formData);
-console.log(response);
-alert('User registered successfully');
-} catch (error) {
-  console.error(error);
-  alert(`Error registering user: ${error.message}`);
-}
+    e.preventDefault();
+    
+    try {
+        const response = await axios.post('http://localhost:3001/api/register', formData);
+        console.log(response);
+        alert('User registered successfully');
+    } catch (error) {
+        console.error(error);
+        alert(`Error registering user: ${error.message}`);
+    }
 };
 
-return (
-<div style={styles.loginPageContainer}>
-<Header />
-<div style={styles.formContainer}>
-<form onSubmit={handleRegister} style={styles.loginPaneContainer}>
-<h1 style={styles.loginHeader}>Register</h1>
-<InputForm
-         style={styles.formPadding}
-         type="text"
-         name="first_name"
-         placeholder="First Name"
-         onChangeHandler={handleChange}
-       />
-<InputForm
-         style={styles.formPadding}
-         type="text"
-         name="last_name"
-         placeholder="Last Name"
-         onChangeHandler={handleChange}
-       />
-<InputForm
-         style={styles.formPadding}
-         type="email"
-         name="email"
-         placeholder="Email"
-         onChangeHandler={handleChange}
-       />
-<InputForm
-         style={styles.formPadding}
-         type="password"
-         name="password"
-         placeholder="Password"
-         onChangeHandler={handleChange}
-       />
-<InputForm
-         style={styles.formPadding}
-         type="text"
-         name="phone_number"
-         placeholder="Phone Number"
-         onChangeHandler={handleChange}
-       />
-<InputForm
-         style={styles.formPadding}
-         type="text"
-         name="address"
-         placeholder="Address"
-         onChangeHandler={handleChange}
-       />
-<Button style={styles.buttonPadding} title="Register" />
-<p style={styles.legalStatement}>
-By signing up, you agree to our
-<br />
-<strong>Terms and Conditions</strong>
-</p>
-</form>
-</div>
-</div>
-);
+    return (
+        <div style={styles.loginPageContainer}>
+            <Header/>
+            <div style={styles.formContainer}>
+                <form onSubmit={handleRegister} style={styles.loginPaneContainer}>
+                    <h1 style={styles.loginHeader}>Register</h1>
+                    <InputForm
+                        style={styles.formPadding}
+                        type="text"
+                        name="first_name"
+                        placeholder="First Name"
+                        onChangeHandler={handleChange}
+                    />
+                    <InputForm
+                        style={styles.formPadding}
+                        type="text"
+                        name="last_name"
+                        placeholder="Last Name"
+                        onChangeHandler={handleChange}
+                    />
+                    <InputForm
+                        style={styles.formPadding}
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        onChangeHandler={handleChange}
+                    />
+                    <InputForm
+                        style={styles.formPadding}
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        onChangeHandler={handleChange}
+                    />
+                    <InputForm
+                        style={styles.formPadding}
+                        type="text"
+                        name="phone_number"
+                        placeholder="Phone Number"
+                        onChangeHandler={handleChange}
+                    />
+                    <InputForm
+                        style={styles.formPadding}
+                        type="text"
+                        name="address"
+                        placeholder="Address"
+                        onChangeHandler={handleChange}
+                    />
+                    <Button style={styles.buttonPadding} title="Register"/>
+                    <p style={styles.legalStatement}>
+                        By signing up, you agree to our
+                        <br/>
+                        <strong>Terms and Conditions</strong>
+                        </p>
+                </form>
+            </div>
+        </div>
+    );
 }
 
-// Add Style Sheet here for Simplicity
 const styles = {
-// ... your styles object
-loginPageContainer : {
-backgroundColor: 'black',
-minHeight: '100vh',
-minWidth: '100vh',
-display : 'flex',
-alignItems : 'center',
-flexDirection:'column'
-},
-loginPaneContainer:{
-backgroundColor : '#1A1B1E',
-height : '400px',
-width : '400px',
-borderRadius : 10,
-display: 'flex',
-flexDirection : 'column',
-padding: '15px',
-alignItems: 'center',
-justifySelf:'center'
-},
-loginHeader : {
-color: 'white'
-},
-formPadding : {
-marginTop : 25
-},
-buttonPadding : {
-marginTop: 50,
-marginBottom: 10
-},
-legalStatement : {
-width : '75%',
-textAlign : 'center',
-color: 'white',
-fontSize: 13
-},
-formContainer : {
-width:'100%',
-height: '100vh',
-display:'flex',
-justifyContent:'center',
-justifySelf:'center',
-alignItems:'center'
-  }
+    loginPageContainer : {
+        backgroundColor: 'black',
+        minHeight: '100vh',
+        minWidth: '100vh',
+        display : 'flex',
+        alignItems : 'center',
+        flexDirection:'column'
+    },
+    loginPaneContainer:{
+        backgroundColor : '#1A1B1E',
+        height : '600px',
+        width : '400px',
+        borderRadius : 10,
+        display: 'flex',
+        flexDirection : 'column',
+        padding: '15px',
+        alignItems: 'center',
+        justifySelf:'center'
+    },
+    loginHeader : {
+        color: 'white'
+    },
+    formPadding : {
+        marginTop : 25
+    },
+    buttonPadding : {
+        marginTop: 30,
+    },
+    legalStatement : {
+        width : '75%',
+        textAlign : 'center',
+        color: 'white',
+        fontSize: 13
+    },
+    formContainer : {
+        width:'100%',
+        height: '100vh',
+        display:'flex',
+        justifyContent:'center',
+        justifySelf:'center',
+        alignItems:'center'
+    }
 };
 
 export default RegisterPage;
